02848b5231b4b2bf341c49f6c0666746c2315feb
Vaneesa Cuevas <vaneesacuevas@MacBook-Air-de-Vaneesa.local>
2025-08-07 14:21:27 -0600

    Delete Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
-import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -20,7 +19,6 @@ function App() {
             <Hero />
             <About />
             <Skills />
-            <Projects />
             <Experience />
             <Contact />
           </main>
